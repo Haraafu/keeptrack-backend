12deca167fd249ecf35c790bc79c89379ce633a2
Falah Andhesryo <falahand@gmail.com>
2025-05-04 23:02:07 +0700

    vercel.json

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
